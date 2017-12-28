1b8330974601873714baf33a7d11a0bda3ecc7c6
Jean <jeanlucafp@gmail.com>
2017-12-27 23:14:38 -0200

    ta ficando massa

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,11 +1,26 @@
 module.exports = function(app){
     app.get('/formulario', function(req,res){
-        res.render('admin/form_add_noticia');
+        res.render('admin/form_add_noticia', {validacao :{}, noticia :{}});
     });
 
     app.post('/noticias/salvar', function(req, res){
     	var noticia = req.body;
     	
+        req.assert('titulo', 'Título é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+        req.assert('autor', 'Autor é obrigatório').notEmpty();
+        req.assert('data_noticia', 'Data é obrigatório').notEmpty().isDate({format: 'YYYY-MM-DD'});
+        req.assert('noticia', 'Notícia é obrigatório').notEmpty();
+
+        //var error = req.getValidationResult();
+        var error = req.validationErrors();
+
+        if(error){
+            res.render('admin/form_add_noticia', {validacao: error,  noticia: noticia});
+            return;
+        }
+
     	var connection = app.config.database();
     	var noticiasModel = new app.app.models.NoticiasDAO(connection);
 
--- config/config.js
+++ config/config.js
@@ -4,11 +4,13 @@
 var app = require('express')();
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
 app.use(bodyParser.urlencoded({extended : true}));
+app.use(expressValidator());
 
 consign()
 	.include('app/routes')
