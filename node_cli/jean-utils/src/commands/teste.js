a5ebacce1dc23dbb1fb05d6e3dccbda697a2a776
Jean <jeanlucafp@gmail.com>
2019-07-02 21:58:27 -0300

    Update teste.js
    ..

--- node_cli/jean-utils/src/commands/teste.js
+++ node_cli/jean-utils/src/commands/teste.js
@@ -1,10 +1,10 @@
+//https://www.npmjs.com/package/shelljs
+const shell = require('shelljs');
 
 module.exports = {
-    name: 'jean-utils',
-    run: async toolbox => {
-      const { print, parameters  } = toolbox
-      console.log(parameters)
-      print.info('Welcome to your CLI')
+    name: 'mongo',
+    run: async () => {
+      shell.exec(`mongod --dbpath /Users/jeanfernandes/Documents/mongoDB/data/db`)
     }
   }
   
